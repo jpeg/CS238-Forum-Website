e64c4033b2ee9d789fc553ea7ab245991d3f128e
Jason Gassel <pegelite1@gmail.com>
2013-04-25 13:39:07 -0500

    Install script outline done, just needs table setup queries

--- install.php
+++ install.php
@@ -1,3 +1,7 @@
+<?php
+include 'config.php';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -18,10 +22,44 @@ if(isset($_GET['install']))
   session_start();
   session_destroy();
   
+  $success = "<font color=\"gree\">SUCCESS</font><br />\n";
+  $failure = "<font color=\"red\">FAILED</font><br />\n";
+  
   // Setup database
   echo "<p>Installing...</p><p>\n";
   
-  echo "Actually do something here!!!<br />\n"; //TODO
+  echo 'Connect to database: ';
+  $db = mysql_connect($db_server, $db_user, $db_password);
+  if(!$db)
+    echo $failure;
+  else
+  {
+    echo $success;
+    
+    // Drop database to delete old data
+    echo 'Drop old database: ';
+    if(mysql_query("DROP DATABASE $db_database", $db))
+      echo $success;
+    else
+      echo "<font color=\"green\">DNE</font><br />\n";
+    
+    // Create and connect to database
+    echo 'Creating database: ';
+    if(mysql_query("CREATE DATABASE $db_database", $db))
+      echo $success;
+    else
+      echo $failure;
+    echo 'Connecting to database: ';
+    if(!mysql_select_db($db_database, $db))
+      echo $failure;
+    else
+    {
+      echo $success;
+      
+      //TODO setup tables
+    }
+  }
+  mysql_close($db);
   
   echo "</p><p>Done</p>\n";
 }
