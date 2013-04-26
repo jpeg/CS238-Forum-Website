68e3066e808f2975e8ef01ab750437d30862b36c
Jason Gassel <pegelite1@gmail.com>
2013-04-25 22:23:16 -0500

    Added database tables setup to install script

--- install.php
+++ install.php
@@ -18,7 +18,7 @@ if(isset($_GET['install']))
   $dne = "<font color=\"green\">DNE</font><br />\n";
   
   // Setup database
-  echo "<p>Installing...</p><p>\n";
+  echo "<h3>Install Script</h3><p>\n";
   
   echo 'Connect to database: ';
   $db = mysql_connect($db_server, $db_user, $db_password);
@@ -48,11 +48,70 @@ if(isset($_GET['install']))
     {
       echo $success;
       
-      //TODO setup tables
+      // Create database tables
+      echo 'Creating users table: ';
+      if(mysql_query('CREATE TABLE user(uid INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20) UNIQUE NOT NULL, password CHAR(32) NOT NULL, avatar VARCHAR(10), firstName VARCHAR(20), lastName VARCHAR(20))'))
+        echo $success;
+      else
+        echo $failure;
+      
+      echo 'Creating threads table: ';
+      if(mysql_query('CREATE TABLE thread(tid INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, uid INT NOT NULL, title VARCHAR(40) NOT NULL, type INT NOT NULL, question VARCHAR(100), tag VARCHAR(10), FOREIGN KEY (uid) REFERENCES user(uid))'))
+        echo $success;
+      else
+        echo $failure;
+      
+      echo 'Creating posts table: ';
+      if(mysql_query('CREATE TABLE post(pid INT UNIQUE NOT NULL AUTO_INCREMENT, tid INT NOT NULL, uid INT NOT NULL, date DATE NOT NULL, time TIME NOT NULL, text VARCHAR(10000), PRIMARY KEY(pid, tid), FOREIGN KEY (tid) REFERENCES thread(tid), FOREIGN KEY (uid) REFERENCES user(uid))'))
+        echo $success;
+      else
+        echo $failure;
+      
+      echo 'Creating poll options table: ';
+      if(mysql_query('CREATE TABLE poll_option(tid INT NOT NULL, oid INT NOT NULL, option_text VARCHAR(60) NOT NULL, PRIMARY KEY(tid, oid), FOREIGN KEY (tid) REFERENCES thread(tid))'))
+        echo $success;
+      else
+        echo $failure;
+      
+      echo 'Creating poll votes table: ';
+      if(mysql_query('CREATE TABLE poll_vote(tid INT NOT NULL, uid INT NOT NULL, oid INT NOT NULL, PRIMARY KEY(tid, uid), FOREIGN KEY (tid) REFERENCES thread(tid), FOREIGN KEY (uid) REFERENCES user(uid))'))
+        echo $success;
+      else
+        echo $failure;
     }
   }
   mysql_close($db);
   
+  // Avatars
+  echo 'Checking images directory: ';
+  if (!is_dir('images'))
+  {
+    if(mkdir('images'))
+      echo $success;
+    else
+      echo $failure;
+  }
+  else
+    echo $success;
+  
+  echo 'Deleting old avatar directory: ';
+  if (is_dir('images/avatars'))
+  {
+    if(rmdir('images/avatars'))
+      echo $success;
+    else
+      echo $failure;
+  }
+  else
+    echo $dne;
+  
+  echo 'Creating avatar directory: ';
+  if(mkdir('images/avatars'))
+    echo $success;
+  else
+    echo $failure;
+  
+  // Finished
   echo "</p><p>Done</p>\n";
 }
 else
