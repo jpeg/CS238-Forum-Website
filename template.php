fd4c9f46ccefde30ec3829ff9b8d21a5cfa8c009
Jason Gassel <pegelite1@gmail.com>
2013-05-01 01:49:26 -0500

    Made search link color differentiated

--- template.php
+++ template.php
@@ -34,7 +34,7 @@ function template_forum_header()
   <header>
     <ul>
       <li><a href="index.php" id="logo">forum238</a></li>
-      <li><a href="search.php">Search</a></li>
+      <li><a href="search.php" id="search">Search</a></li>
 <?php
   if($_SESSION['uid'] == 0)
   {
